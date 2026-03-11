2af76e55f157422ddfe1fc630a47c4bfabc09a3d
Subarna-01 <subarnabhowmik01@gmail.com>
2026-03-12 02:58:29 +0530

    Added auth in search routes

--- app/modules/search/router.py
+++ app/modules/search/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Query, Depends
 from fastapi.responses import JSONResponse
 from elasticsearch import Elasticsearch
 from app.core.elasticsearch.connection import elasticsearch_connection_manager
+from app.core.security import jwt
 from app.modules.search.service import SearchService
 
 search_router = APIRouter(prefix="/search", tags=["search"])
@@ -9,6 +10,6 @@ search_router = APIRouter(prefix="/search", tags=["search"])
 search_service = SearchService()
 
 @search_router.get("/")
-async def search(q: str = Query(...), page: int = 1, size: int = 10, client: Elasticsearch = Depends(elasticsearch_connection_manager.get_client)) -> JSONResponse:
+async def search(q: str = Query(...), page: int = 1, size: int = 10, decoded_token_data: dict = Depends(jwt.authenticate), client: Elasticsearch = Depends(elasticsearch_connection_manager.get_client)) -> JSONResponse:
     return await search_service.search(q, page, size, client)
      
