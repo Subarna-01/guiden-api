08bcd52c61ae989d0923a8bbab9dbc788e295ff5
Subarna Bhowmik <subarnabhowmik@smcfinance.com>
2026-03-20 18:49:23 +0530

    .

--- app/core/database/dependencies.py
+++ app/core/database/dependencies.py
@@ -1,9 +1,9 @@
 from typing import Generator
 from sqlalchemy.orm import Session
-from app.core.database.connection import db_connection_manager
+from app.core.database.connection import db_conn_manager
 
 def get_db(db_name: str) -> Generator[Session, None, None]:
-    SessionLocal = db_connection_manager.get_session(db_name)
+    SessionLocal = db_conn_manager.get_session(db_name)
 
     db = SessionLocal()
     try:
